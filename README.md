16383bf0bc030fa84426734159f41cd242300968
Maria Eduarda Venturini Wollmann <mduduv@gmail.com>
2024-09-18 11:53:21 -0300

    Primeiro Commit - Projeto Prático 1 (P1)

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_mycatcoffeebar_p1
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/flutter_mycatcoffeebar_p1/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_mycatcoffeebar_p1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
