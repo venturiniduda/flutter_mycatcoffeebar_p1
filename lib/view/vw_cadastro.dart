bf908f6b664b66ba1b113c5d8fd61c893a81808f
Maria Eduarda Venturini Wollmann <mduduv@gmail.com>
2024-09-19 07:53:29 -0300

    Criação View

---
+++ lib/view/vw_cadastro.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CadastroView extends StatefulWidget {
+  const CadastroView({super.key});
+
+  @override
+  State<CadastroView> createState() => _CadastroViewState();
+}
+
+class _CadastroViewState extends State<CadastroView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
