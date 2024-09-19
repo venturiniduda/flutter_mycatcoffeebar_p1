fd9e87693d2b687e0da645ee9ba1256c26ed7274
Maria Eduarda Venturini Wollmann <mduduv@gmail.com>
2024-09-19 08:54:52 -0300

    Ajustes Telas

--- lib/view/vw_login.dart
+++ lib/view/vw_login.dart
@@ -8,26 +8,62 @@ class LoginView extends StatefulWidget {
 }
 
 class _LoginViewState extends State<LoginView> {
+  // Variáveis para Login
+  final GlobalKey<FormState> loginKey = GlobalKey<FormState>();
+  var txtConta = TextEditingController();
+  var txtSenha = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            IconButton(
-              onPressed: () {
-                Navigator.pushNamedAndRemoveUntil(
-                  context,
-                  'home',
-                  (Route<dynamic> route) => false,
-                );
-              },
-              icon: const Icon(Icons.cancel, size: 30.0),
-              color: Colors.red,
-            ),
-            Text('Login'),
-          ],
+        child: Form(
+          key: loginKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              IconButton(
+                onPressed: () {
+                  Navigator.pushNamedAndRemoveUntil(
+                    context,
+                    'home',
+                    (Route<dynamic> route) => false,
+                  );
+                },
+                icon: const Icon(Icons.cancel, size: 30.0),
+                color: Colors.red,
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Text('Login'),
+              TextFormField(
+                controller: txtConta,
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  labelStyle: TextStyle(color: Colors.pink[900]),
+                  hintText: 'Insira seu email',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(50.0),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              TextFormField(
+                controller: txtSenha,
+                decoration: InputDecoration(
+                  labelText: 'Senha',
+                  labelStyle: TextStyle(color: Colors.pink[900]),
+                  hintText: 'Insira sua senha',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(50.0),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
