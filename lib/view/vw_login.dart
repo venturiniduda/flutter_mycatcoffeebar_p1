03814b6fe39a8d0973804dcbd65a5e446371b8ce
Maria Eduarda Venturini Wollmann <mduduv@gmail.com>
2024-09-26 09:15:07 -0300

    Adição Botões e Ajuste Layout - Login

--- lib/view/vw_login.dart
+++ lib/view/vw_login.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+// to do:
+// adicionar "legenda" quando passamos o mouse nos botões
+// diminuir tamanho botão esqueceu a senha e alinhar na esquerda
 
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
@@ -19,50 +24,108 @@ class _LoginViewState extends State<LoginView> {
       body: Center(
         child: Form(
           key: loginKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              IconButton(
-                onPressed: () {
-                  Navigator.pushNamedAndRemoveUntil(
-                    context,
-                    'home',
-                    (Route<dynamic> route) => false,
-                  );
-                },
-                icon: const Icon(Icons.cancel, size: 30.0),
-                color: Colors.red,
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              Text('Login'),
-              TextFormField(
-                controller: txtConta,
-                decoration: InputDecoration(
-                  labelText: 'Email',
-                  labelStyle: TextStyle(color: Colors.pink[900]),
-                  hintText: 'Insira seu email',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(50.0),
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'lib/images/logo.png',
+                  height: 250,
+                  width: 350,
+                ),
+                Text(
+                  'Login',
+                  style: GoogleFonts.reenieBeanie(fontSize: 150),
+                  textAlign: TextAlign.end,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  controller: txtConta,
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                    labelStyle: TextStyle(color: Colors.black),
+                    hintText: 'Insira seu email',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(50.0),
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              TextFormField(
-                controller: txtSenha,
-                decoration: InputDecoration(
-                  labelText: 'Senha',
-                  labelStyle: TextStyle(color: Colors.pink[900]),
-                  hintText: 'Insira sua senha',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(50.0),
+                SizedBox(
+                  height: 15,
+                ),
+                TextFormField(
+                  controller: txtSenha,
+                  decoration: InputDecoration(
+                    labelText: 'Senha',
+                    labelStyle: TextStyle(color: Colors.black),
+                    hintText: 'Insira sua senha',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(50.0),
+                    ),
                   ),
                 ),
-              ),
-            ],
+                SizedBox(
+                  height: 15,
+                ),
+                Row(
+                  children: [
+                    TextButton.icon(
+                      onPressed: () {},
+                      icon: Icon(Icons.key_off),
+                      label: Text('Esqueci a senha'),
+                      iconAlignment: IconAlignment.end,
+                    ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    IconButton(
+                      onPressed: () {
+                        Navigator.pushNamedAndRemoveUntil(
+                          context,
+                          'home',
+                          (Route<dynamic> route) => false,
+                        );
+                      },
+                      icon: const Icon(Icons.cancel, size: 30.0),
+                      color: Colors.red,
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        Navigator.pushNamedAndRemoveUntil(
+                          context,
+                          'cadastro',
+                          (Route<dynamic> route) => false,
+                        );
+                      },
+                      icon: const Icon(Icons.add_circle, size: 30.0),
+                      color: Colors.blue,
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        Navigator.pushNamedAndRemoveUntil(
+                          context,
+                          'cardapio',
+                          (Route<dynamic> route) => false,
+                        );
+                      },
+                      icon: const Icon(Icons.check_circle, size: 30.0),
+                      color: Colors.green,
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
