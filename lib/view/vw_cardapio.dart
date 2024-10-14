70042743339b336aafeae9ced9669a9b95813110
Maria Eduarda Venturini Wollmann <mduduv@gmail.com>
2024-10-13 22:08:50 -0300

    Estruturação Cardápio

--- lib/view/vw_cardapio.dart
+++ lib/view/vw_cardapio.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:intl/intl.dart';
+import '../service/srv_dados.dart';
 
 // to do: organizar em um ListView e, nos botoes de categoria, quando clicados,
 // pular para o indice que corresponde a essa categoria
 
 // botão logout -> levar p login
+
+final DadosService srv = GetIt.instance<DadosService>();
 
 class CardapioView extends StatefulWidget {
   const CardapioView({super.key});
@@ -16,8 +21,53 @@ class _CardapioViewState extends State<CardapioView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ListView(),
+      appBar: AppBar(
+        title: Text(
+          'Cardápio',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.purple.shade900,
+        automaticallyImplyLeading: false,
+        actions: [
+          IconButton(
+            padding: EdgeInsets.only(right: 30),
+            onPressed: () {
+              Navigator.pushReplacementNamed(context, 'inserir');
+            },
+            icon: Icon(Icons.add, color: Colors.white),
+          ),
+        ],
+      ),
+      backgroundColor: Colors.purple.shade50,
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          children: [
+            Row(),
+            ListView.builder(
+              itemCount: srv.cardapio.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  child: ListTile(
+                    title: Text(srv.cardapio[index].nome,
+                        style: TextStyle(fontSize: 22)),
+                    subtitle: Text(
+                      NumberFormat('#,##0.00')
+                          .format(srv.cardapio[index].valor),
+                      style:
+                          TextStyle(fontSize: 14, fontStyle: FontStyle.italic),
+                    ),
+                    trailing: Icon(Icons.arrow_right),
+                    onTap: () {
+                      Navigator.pushNamed(context, 'detailsfood',
+                          arguments: index);
+                    },
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
