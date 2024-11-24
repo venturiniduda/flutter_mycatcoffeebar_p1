89e0cf5f9e0e203f3fbab6a3beb03b25c288a539
Maria Eduarda Venturini Wollmann <mduduv@gmail.com>
2024-11-24 18:48:13 -0300

    Voltando versões view

--- lib/view/vw_cadastro.dart
+++ lib/view/vw_cadastro.dart
@@ -1,6 +1,12 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_mycatcoffeebar_p1/controller/ct_login.dart';
+import 'package:flutter_mycatcoffeebar_p1/service/srv_dados.dart';
+import 'package:get_it/get_it.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:email_validator/email_validator.dart';
+
+final DadosService srv = GetIt.instance<DadosService>();
 
 class CadastroView extends StatefulWidget {
   CadastroView({super.key});
@@ -10,6 +16,7 @@ class CadastroView extends StatefulWidget {
 }
 
 class _CadastroViewState extends State<CadastroView> {
+// Variáveis para Login
   final GlobalKey<FormState> cadastroKey = GlobalKey<FormState>();
   var txtNome = TextEditingController();
   var txtSobrenome = TextEditingController();
@@ -19,182 +26,203 @@ class _CadastroViewState extends State<CadastroView> {
   var txtSenhaconf = TextEditingController();
 
   @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return ScreenUtilInit(
-      designSize: const Size(200, 360), // Dimensões base do design
-      minTextAdapt: true,
-      builder: (context, child) {
-        return Scaffold(
-          body: Center(
-            child: Form(
-              key: cadastroKey,
-              child: Padding(
-                padding: EdgeInsets.symmetric(horizontal: 30.w, vertical: 50.h),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Image.asset(
-                      'lib/images/logo.png',
-                      height: 70.h,
-                      width: 70.w,
-                    ),
-                    Text(
-                      'Cadastro',
-                      style: GoogleFonts.reenieBeanie(fontSize: 70.sp),
-                      textAlign: TextAlign.center,
-                    ),
-                    SizedBox(height: 20.h),
-                    TextFormField(
-                      controller: txtNome,
-                      decoration: InputDecoration(
-                        labelText: 'Nome',
-                        labelStyle: TextStyle(fontSize: 16.sp),
-                        hintText: 'Insira seu nome',
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(50.r),
-                        ),
+    return Scaffold(
+      body: Center(
+        child: Form(
+          key: cadastroKey,
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(30, 50, 30, 50),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'lib/images/logo.png',
+                  height: 70,
+                  width: 70,
+                ),
+                Text(
+                  'Cadastro',
+                  style: GoogleFonts.reenieBeanie(fontSize: 70),
+                  textAlign: TextAlign.end,
+                ),
+                TextFormField(
+                    controller: txtNome,
+                    decoration: InputDecoration(
+                      labelText: 'Nome',
+                      labelStyle: TextStyle(color: Colors.black),
+                      hintText: 'Insira seu nome',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(50.0),
                       ),
-                      validator: (txtNome) {
-                        if (txtNome == null || txtNome.isEmpty) {
-                          return 'Informe um Nome';
+                    ),
+                    validator: (txtNome) {
+                      if (txtNome == null) {
+                        return 'Informe um Nome';
+                      } else if (txtNome.isEmpty) {
+                        return 'Informe um Nome';
+                      }
+                      return null;
+                    }),
+                SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                    controller: txtSobrenome,
+                    decoration: InputDecoration(
+                      labelText: 'Sobrenome',
+                      labelStyle: TextStyle(color: Colors.black),
+                      hintText: 'Insira seu sobrenome',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(50.0),
+                      ),
+                    ),
+                    validator: (txtSobrenome) {
+                      if (txtSobrenome == null) {
+                        return 'Informe um Sobrenome';
+                      } else if (txtSobrenome.isEmpty) {
+                        return 'Informe um Sobrenome';
+                      }
+                      return null;
+                    }),
+                SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                    controller: txtCel,
+                    decoration: InputDecoration(
+                      labelText: 'Celular',
+                      labelStyle: TextStyle(color: Colors.black),
+                      hintText: 'Digite o número de celular (Ex: 16991234567)',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(50.0),
+                      ),
+                    ),
+                    validator: (txtCel) {
+                      if (txtCel == null) {
+                        return 'Informe um Celular';
+                      } else if (txtCel.isEmpty) {
+                        return 'Informe um Celular';
+                      } else {
+                        final regex = RegExp(r'^\d{2}\d{9}$');
+                        if (!regex.hasMatch(txtCel)) {
+                          return 'Número de celular inválido';
                         }
                         return null;
-                      },
-                    ),
-                    SizedBox(height: 10.h),
-                    TextFormField(
-                      controller: txtSobrenome,
-                      decoration: InputDecoration(
-                        labelText: 'Sobrenome',
-                        labelStyle: TextStyle(fontSize: 16.sp),
-                        hintText: 'Insira seu sobrenome',
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(50.r),
-                        ),
-                      ),
-                      validator: (txtSobrenome) {
-                        if (txtSobrenome == null || txtSobrenome.isEmpty) {
-                          return 'Informe um Sobrenome';
-                        }
-                        return null;
-                      },
-                    ),
-                    SizedBox(height: 10.h),
-                    TextFormField(
-                      controller: txtCel,
-                      decoration: InputDecoration(
-                        labelText: 'Celular',
-                        labelStyle: TextStyle(fontSize: 16.sp),
-                        hintText:
-                            'Digite o número de celular (Ex: 16991234567)',
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(50.r),
-                        ),
-                      ),
-                      validator: (txtCel) {
-                        if (txtCel == null || txtCel.isEmpty) {
-                          return 'Informe um Celular';
-                        } else {
-                          final regex = RegExp(r'^\d{2}\d{9}$');
-                          if (!regex.hasMatch(txtCel)) {
-                            return 'Número de celular inválido';
-                          }
-                        }
-                        return null;
-                      },
-                    ),
-                    SizedBox(height: 10.h),
-                    TextFormField(
-                      controller: txtConta,
-                      decoration: InputDecoration(
+                      }
+                    }),
+                SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                    controller: txtConta,
+                    decoration: InputDecoration(
                         labelText: 'Email',
-                        labelStyle: TextStyle(fontSize: 16.sp),
+                        labelStyle: TextStyle(color: Colors.black),
                         hintText: 'Insira seu email',
                         border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(50.r),
-                        ),
+                          borderRadius: BorderRadius.circular(50.0),
+                        )),
+                    validator: (txtConta) {
+                      if (txtConta == null) {
+                        return 'Informe um Email';
+                      } else if (txtConta.isEmpty) {
+                        return 'Informe um Email';
+                      }
+                      return null;
+                    }),
+                SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                    controller: txtSenha,
+                    decoration: InputDecoration(
+                      labelText: 'Senha',
+                      labelStyle: TextStyle(color: Colors.black),
+                      hintText: 'Insira sua senha',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(50.0),
                       ),
-                      validator: (txtConta) {
-                        if (txtConta == null || txtConta.isEmpty) {
-                          return 'Informe um Email';
-                        }
-                        return null;
-                      },
                     ),
-                    SizedBox(height: 10.h),
-                    TextFormField(
-                      controller: txtSenha,
-                      decoration: InputDecoration(
-                        labelText: 'Senha',
-                        labelStyle: TextStyle(fontSize: 16.sp),
-                        hintText: 'Insira sua senha',
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(50.r),
-                        ),
+                    validator: (txtSenha) {
+                      if (txtSenha == null) {
+                        return 'Informe um Senha';
+                      } else if (txtSenha.isEmpty) {
+                        return 'Informe um Senha';
+                      }
+                      return null;
+                    }),
+                SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                    controller: txtSenhaconf,
+                    decoration: InputDecoration(
+                      labelText: 'Confirmação de Senha',
+                      labelStyle: TextStyle(color: Colors.black),
+                      hintText: 'Insira sua senha novamente',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(50.0),
                       ),
-                      validator: (txtSenha) {
-                        if (txtSenha == null || txtSenha.isEmpty) {
-                          return 'Informe uma Senha';
-                        }
-                        return null;
-                      },
                     ),
-                    SizedBox(height: 10.h),
-                    TextFormField(
-                      controller: txtSenhaconf,
-                      decoration: InputDecoration(
-                        labelText: 'Confirmação de Senha',
-                        labelStyle: TextStyle(fontSize: 16.sp),
-                        hintText: 'Insira sua senha novamente',
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(50.r),
-                        ),
-                      ),
-                      validator: (txtSenhaconf) {
-                        if (txtSenhaconf == null || txtSenhaconf.isEmpty) {
-                          return 'Confirme sua senha';
-                        } else if (txtSenhaconf != txtSenha.text) {
-                          return 'As senhas estão diferentes!';
-                        }
-                        return null;
+                    validator: (txtSenhaconf) {
+                      if (txtSenhaconf == null || txtSenhaconf.isEmpty) {
+                        return 'Confirme sua senha';
+                      } else if (txtSenhaconf != txtSenha.text) {
+                        return 'As senhas estão diferentes!';
+                      }
+                      return null;
+                    }),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    IconButton(
+                      onPressed: () {
+                        Navigator.pushNamedAndRemoveUntil(
+                          context,
+                          'login',
+                          (Route<dynamic> route) => false,
+                        );
                       },
+                      icon: const Icon(Icons.cancel, size: 50.0),
+                      color: Colors.red,
+                      tooltip: 'Cancelar',
                     ),
-                    SizedBox(height: 20.h),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        IconButton(
-                          onPressed: () {
-                            Navigator.pushNamedAndRemoveUntil(
+                    SizedBox(
+                      height: 15,
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        if (cadastroKey.currentState!.validate()) {
+                          // salvando dados do usuário e criando conta
+                          LoginController().criarConta(
                               context,
-                              'login',
-                              (Route<dynamic> route) => false,
-                            );
-                          },
-                          icon: Icon(Icons.cancel, size: 50.sp),
-                          color: Colors.red,
-                          tooltip: 'Cancelar',
-                        ),
-                        SizedBox(width: 20.w),
-                        IconButton(
-                          onPressed: () {
-                            if (cadastroKey.currentState!.validate()) {
-                              // Lógica para cadastrar o usuário
-                            }
-                          },
-                          icon: Icon(Icons.check_circle, size: 50.sp),
-                          color: Colors.green,
-                          tooltip: 'Cadastrar',
-                        ),
-                      ],
+                              txtNome.text,
+                              txtSobrenome.text,
+                              txtCel.text,
+                              txtConta.text,
+                              txtSenha.text);
+                        }
+                      },
+                      icon: const Icon(Icons.check_circle, size: 50.0),
+                      color: Colors.green,
+                      tooltip: 'Cadastrar',
                     ),
                   ],
                 ),
-              ),
+              ],
             ),
           ),
-        );
-      },
+        ),
+      ),
     );
   }
 }
